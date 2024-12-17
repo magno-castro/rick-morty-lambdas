57e50ab71647cf4805522e9d2b0ed046bc253401
Magno Castro Moraes <magno.castro@niduu.com>
2024-12-16 23:19:07 -0300

    fix: apply some fixes to get characters and create character lambdas

--- src/create-character/src/create-character.js
+++ src/create-character/src/create-character.js
@@ -8,42 +8,49 @@ const docClient = DynamoDBDocumentClient.from(dbClient);
 
 export const handler = async (event) => {
   try {
-    const character = event;
-    
-    const requiredFields = ['name', 'status', 'species', 'gender', 'origin', 'location', 'image'];
-    const missingFields = requiredFields.filter(field => !character[field]);
-    
+    const character = JSON.parse(event.body);
+
+    const requiredFields = [
+      "name",
+      "status",
+      "species",
+      "gender",
+      "origin",
+      "location",
+    ];
+    const missingFields = requiredFields.filter((field) => !character[field]);
+
     if (missingFields.length > 0) {
       return {
         statusCode: 400,
-        body: {
-          message: `Missing required fields: ${missingFields.join(', ')}`
-        }
+        body: JSON.stringify({
+          message: `Missing required fields: ${missingFields.join(", ")}`,
+        }),
       };
     }
 
     const timestamp = Date.now();
-    const randomNumber = Math.floor(Math.random() * 1000); 
+    const randomNumber = Math.floor(Math.random() * 1000);
     character.id = Number(`${timestamp}${randomNumber}`);
     character.source = "filler";
 
     const command = new PutCommand({
       TableName: TABLE_NAME,
-      Item: character
+      Item: character,
     });
 
     await docClient.send(command);
 
     return {
       statusCode: 201,
-      body: character
+      body: JSON.stringify(character),
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: {
-        message: error.message
-      }
+      body: JSON.stringify({
+        message: error.message,
+      }),
     };
   }
 };
